8b1013a047976631eb71fccfb88c0a95c7d3d26a
Denlich <muchalovych@gmail.com>
2023-05-18 13:06:13 +0300

    Client: Build profile page

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Heading from "../../components/Heading";
+import Text from "../../components/Text";
+import user from "../../assets/I_end.png";
+
+import styles from "./index.module.css";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div className={styles.container}>
+      <img className={styles.user} src={user} />
+      <div className={styles.info}>
+        <Heading>Denys Mykhailovych</Heading>
+        <Text color="grey">@Denlich</Text>
+      </div>
+    </div>
+  );
 };
 
 export default index;
